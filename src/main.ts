a6c63f929568d7469d1d56bae2d1601fbb1266c3
Tychio <code@tychio.net>
2022-10-10 21:46:50 +0800

    setup swapper for api document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Tyapio")
+    .setDescription("Tychio's personal APIs documents")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(configService.get("PORT", process.env.PORT || 3000))
+    .then(v => console.log('Tyapio APIs Server Ready'))
+    .catch(err => console.log('Tyapio APIs Server broken', JSON.stringify(err)));
 }
 bootstrap();
