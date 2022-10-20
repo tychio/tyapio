b970fba30ed516f0540a197bf34f13134fb1ff13
Tychio <code@tychio.net>
2022-10-20 20:12:03 +0800

    setup type orm for database

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,16 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 export default () => ({
+  prod: process.env.ENV === 'prod',
   port: parseInt(process.env.PORT, 10) || 3000,
   auth: {
     secret: process.env.JWT_SECRET
+  },
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ?? 3306,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
   }
 });
--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import config from "../config";
@@ -6,6 +7,7 @@ import { AuthModule } from "../auth/auth.module";
 
 import { ProfileService } from './services/profile.service';
 import { ProfileController } from './profile.controller';
+import { Profile } from './entities/profile.entity';
 
 @Module({
   imports: [
@@ -13,6 +15,22 @@ import { ProfileController } from './profile.controller';
     ConfigModule.forRoot({
       load: [config],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('db.host'),
+        port: configService.get('db.port'),
+        username: configService.get('db.user'),
+        password: configService.get('db.pass'),
+        database: configService.get('db.name'),
+        synchronize: !configService.get('prod'),
+        entities: [
+          Profile
+        ]
+      })
+    })
   ],
   controllers: [ProfileController],
   providers: [
