61319238d5b61a6b18d57ab949230d0f6105090e
Tychio <code@tychio.net>
2022-10-20 19:37:22 +0800

    decorate profile apis

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,18 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProfileService } from './services/profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('profile')
+@ApiTags("Profile")
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Create profile" })
   create(@Body() createProfileDto: CreateProfileDto) {
     return this.profileService.create(createProfileDto);
   }
 
   @Get()
+  @ApiQuery({
+    name: "page",
+    type: "number",
+  })
+  @ApiQuery({
+    name: "per_page",
+    type: "number",
+  })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Get all profiles" })
   findAll() {
     return this.profileService.findAll();
   }
@@ -23,11 +40,17 @@ export class ProfileController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Update profile" })
   update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
     return this.profileService.update(+id, updateProfileDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Remove profile" })
   remove(@Param('id') id: string) {
     return this.profileService.remove(+id);
   }
